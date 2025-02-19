84fc17b4d084b4d7d8c5f6a28c797981a3690873
krud3 <juan.sebastian.molina@correounivalle.edu.co>
2025-02-19 15:04:03 -0500

    implemented api.ts, first and basic http consumer

---
+++ front/src/lib/api.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://todo.com/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export const getData = async (endpoint: string) => {
+  try {
+    const response = await api.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+    throw error;
+  }
+};
+
+export const postData = async (endpoint: string, data: any) => {
+  try {
+    const response = await api.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error posting data: ', error);
+    throw error;
+  }
+};
+
+export const deleteData = async (endpoint: string) => {
+  try {
+    const response = await api.delete(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting data: ', error);
+    throw error;
+  }
+};
+
+// dunno if putData is necessary
+export const putData = async (endpoint: string, data: any) => {
+  try {
+    const response = await api.put(endpoint, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error putting data: ', error);
+    throw error;
+  }
+};
